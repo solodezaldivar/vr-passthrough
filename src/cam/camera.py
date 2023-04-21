4ba384a2b175e679907ad0de694988123c0822db
AlainKueng <45451714+alainkueng@users.noreply.github.com>
2023-04-21 15:37:51 +0200

    Added new Api Call for objects in room

---
+++ src/cam/camera.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import torch
+import json
+
+model = YOLO("yolov8x.pt")
+
+
+results = model(source = "0", show = False, device = torch.device("cuda:0") ,stream = True, verbose = False)
+for result in results:
+    boxes = result.boxes
+    lables = {"content": []}
+    for box in boxes:
+        lables['content'].append(model.names.get(box.cls.item()))
+
+    with open("objects.json", "w") as outfile:
+        # write the dictionary to the file in JSON format
+        json.dump(lables, outfile)
