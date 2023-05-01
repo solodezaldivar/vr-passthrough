6d6ca73cd6a235ef6e638ef2f7f41c0659b3c953
solodezaldivar <ramon.solodezaldivar@uzh.ch>
2023-05-01 15:36:14 +0200

    feat: Change from sound tracking to sound localization
    - Improves accuracy of coordinates
    - Add energy threshold
    - After 300ms without sound, coordinates are set to null

--- src/server/filetransfer/file_transfer.py
+++ src/server/filetransfer/file_transfer.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.get("/directionFile/")
 def get_file():
     try:
-        return send_file(os.environ.get('PATH_TO_JSON_FILE'))
+        return send_file(os.environ.get('COORDINATES_FILE'))
     except Exception as e:
         return str(e)
 
@@ -17,7 +17,7 @@ def get_file():
 @app.get("/objectsFile/")
 def get_objects():
     try:
-        return send_file(os.environ.get('PATH_TO_JSON_FILE'))
+        return send_file(os.environ.get('COORDINATES_FILE'))
     except Exception as e:
         return str(e)
 
--- src/server/socket_server.py
+++ src/server/socket_server.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 HOST = os.environ.get('IP')
-PORT = os.environ.get('PORT')
+PORT = int(os.environ.get('SSL_PORT'))
+
 BUFFER_SIZE = 296
 remainingTrack = ''
 stream = ''
 string = ''
 active_source = None
+last_time_stamp = 0
 
 
 def create_json(x=None, y=None, z=None):
@@ -23,22 +25,65 @@ def create_json(x=None, y=None, z=None):
             }
         ]
     }
-    #print(sounds_dict)
+
+    print(sounds_dict)
     with open(os.environ.get('PATH_TO_JSON_FILE'), 'w') as outfile:
         json.dump(sounds_dict, outfile)
-        
-def process(msg):
-    data = ''
+
+def create_json_with_energy(x=None, y=None, z=None, e=None):
+    sounds_dict = {
+        "sounds": [
+            {
+                "direction": {"x": x, "y": y, "z": z, "e":e},
+                "type": "VOICE"
+            }
+        ]
+    }
+
+    print(sounds_dict)
+    with open(os.environ.get('PATH_TO_JSON_FILE'), 'w') as outfile:
+        json.dump(sounds_dict, outfile)
+
+def processSSL(msg):
     global active_source
+    global last_time_stamp
+    data = ''
+    try:
+        data = json.loads(msg)
+        # TODO iterate over 4 energy sources and compute mean for better accuracy?
+        active_source = data['src'][0]
+
+        if active_source['E'] > 0.3:
+            last_time_stamp = data['timeStamp']
+            x = active_source['x']
+            y = active_source['y']
+            z = active_source['z']
+            e = active_source['E']
+            create_json(x, y, z)
+    
+        elif data['timeStamp'] - last_time_stamp > 300:
+            print(data['timeStamp'] - last_time_stamp)
+            active_source = None
+            create_json()
+
+    except:
+        pass
+
+
+
+def processSST(msg):
+    global active_source
+    data = ''
     try:
         data = json.loads(msg)
         for source in data['src']:
-            if source['activity'] > 0.2:
+            
+            if source['activity'] > 0.6:
                 if not active_source:
                     if source['id'] != 0:
-                        active_source = source            
-
-                elif source['id'] == active_source['id']:
+                        active_source = source
+                    
+                elif source['id'] is active_source['id']:
                     active_source = source
 
                 elif source['id'] is not active_source['id']:
@@ -51,31 +96,11 @@ def process(msg):
                 create_json(x, y, z)
                 
             else:
+                active_source = None
                 create_json()
 
     except:
-        print("ERROR:JSON LOADS")
-        
-
-def min_len(strs):
-    if (len(strs) < 2):
-        remainingTrack = stream
-        return
-
-
-def remaining(strs):
-    for index, string in enumerate(strs):
-        if (index == len(strs)-1):
-            remainingTrack = string
-            return
-
-        if (string[0] != '{'):
-            string = '{' + string
-
-        if (string[len(string)-2] != '}'):
-            if (string[len(string)-3] != '}'):
-                string = string + '}'
-
+        pass
 
 def server():
     remainingTrack = ''
@@ -111,7 +136,7 @@ def server():
                     if(string[len(string)-3] != '}'):
                         string = string + '}'
 
-    process(string)
+        processSSL(string)
 
 
 if __name__ == '__main__':
